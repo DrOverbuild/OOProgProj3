20f58078c4dac561463b174ab8473f4e7727dd39
Jasper Reddin <jasper@jasperreddin.com>
2020-04-09 12:02:10 -0500

    Finish main table load

--- src/proj3/Project3.java
+++ src/proj3/Project3.java
@@ -1,12 +1,15 @@
 package proj3;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.function.Consumer;
 
 import javax.servlet.RequestDispatcher;
@@ -15,6 +18,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import patientpredictor.Patient;
 import patientpredictor.PatientCollection;
@@ -29,7 +33,7 @@ public class Project3 extends HttpServlet {
 	public final static String CONN_URL = "jdbc:mysql://localhost:3306/patients?user=reader&password=readDBdat@";
 	
 	// table record structure:
-	// |id|result|prediction|protein1|...|protein4776|
+	// |id|result|prediction|proteins|
 	
 	Connection conn = null;
        
@@ -47,6 +51,16 @@ public class Project3 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		if (request.getSession().getAttribute("signed_in") != null && (boolean)request.getSession().getAttribute("signed_in")) {
+			signedIn(request);
+		} else {
+			request.setAttribute("error", "Please log in.");
+		}
+		
+		// send to jsp
+		RequestDispatcher rd = request.getRequestDispatcher("/patients.jsp");
+		rd.forward(request,response);  //forwarded to patients.jsp
 	}
 
 	/**
@@ -57,27 +71,26 @@ public class Project3 extends HttpServlet {
 //		doGet(request, response);
 		// check password
 		
-		if (request.getParameter("username") != null && request.getParameter("password") != null) {
-			// normally I would be checking this with a database but I don't have time to set the user database up
-			// plus, it's not required so I'm not going to worry about it
-			if (request.getParameter("username").equals("md") && request.getParameter("password").equals("pw")) {
-				request.getSession().setAttribute("signed_in", true);
-			} else {
-				request.setAttribute("error", "Username or password is incorrect.");
-			}
-		} else {
-			request.setAttribute("error", "Username or password is incorrect.");
-		}
-		
-		if (request.getSession().getAttribute("signed_in") != null && (boolean)request.getSession().getAttribute("sign_in")) {
+		if (request.getSession().getAttribute("signed_in") != null && (boolean)request.getSession().getAttribute("signed_in")) {
 			signedIn(request);
 		} else {
-			// require log in.
+			if (request.getParameter("username") != null && request.getParameter("password") != null) {
+				// normally I would be checking this with a database but I don't have time to set the user database up
+				// plus, it's not required so I'm not going to worry about it
+				if (request.getParameter("username").equals("md") && request.getParameter("password").equals("pw")) {
+					request.getSession().setAttribute("signed_in", true);
+					signedIn(request);
+				} else {
+					request.setAttribute("error", "Username or password is incorrect.");
+				}
+			} else {
+				request.setAttribute("error", "Please log in.");
+			}
 		}
 		
 		// send to jsp
-		RequestDispatcher rd=request.getRequestDispatcher("/patients.jsp");
-		rd.forward(request,response);  //forwarded to welcome.jsp
+		RequestDispatcher rd = request.getRequestDispatcher("/patients.jsp");
+		rd.forward(request,response);  //forwarded to patients.jsp
 	}
 	
 	protected void makeDBConnection() {
@@ -102,32 +115,52 @@ public class Project3 extends HttpServlet {
 			request.setAttribute("error", "Error connecting to database (check console)");
 			return;
 		}
-					
+		
+		// send data from file to database
 		importPatientsIntoDatabase(request);
 		
+		// build table of patients
 		loadPatients(request);	
 	}
 	
 	protected void importPatientsIntoDatabase(HttpServletRequest request) {
-		try {
-			DatabaseMetaData meta = conn.getMetaData();
-		      ResultSet res = meta.getTables(null, null, null, new String[] {"TABLE"});
-		      System.out.println("List of tables: ");
-		      while (res.next()) {
-		         System.out.println("   "+res.getString("TABLE_CAT")
-		           + ", "+res.getString("TABLE_SCHEM")
-		           + ", "+res.getString("TABLE_NAME")
-		           + ", "+res.getString("TABLE_TYPE")
-		           + ", "+res.getString("REMARKS"));
-		      }
-		// check if table exists and has the right schema
-		// - fix if needed
-		
-		// check if patients need to be loaded in
-		// if so:
-		//  - new patients collection from file
-		//  - for each patient:
-		//    - append to stringbuilder record for that patient
+		try { 
+			// check if patients need to be loaded in (empty table)
+			PreparedStatement st = conn.prepareStatement("select COUNT(*) from patients.patients");
+			ResultSet rs = st.executeQuery();
+			rs.first();
+			int count = rs.getInt(1);
+			if (count == 0) {
+				st.close();
+				rs.close();
+				// do import
+				
+				// load from file
+				PatientCollection patientCollection = new PatientCollection(this.getServletContext().getRealPath("/data.csv"));
+				
+				StringBuilder statement = new StringBuilder("INSERT INTO `patients` (`ID`, `RESULT`, `PRED`, `PROTEINS`) VALUES ");
+				String prefix = "";
+				
+				// iterate through loaded patients
+				for (Patient p: patientCollection.list()) {
+					String id = p.getPatientID();
+					String result = p.getTreatmentResults();
+					String pred = p.getPrediction();
+					
+					// create string array and join proteins separated by commas
+					String[] sarr = Arrays.stream(p.getProteins()).mapToObj(String::valueOf).toArray(String[]::new);
+					String proteins = String.join(",", sarr);
+					
+					statement.append(prefix);
+					prefix = ", ";
+					
+					statement.append("('").append(id).append("','").append(result).append("','").append(pred).append("','")
+						.append(proteins).append("')");
+				}
+				
+				st = conn.prepareStatement(statement.toString());
+				st.execute();
+			}
 		} catch (SQLException e){
 			e.printStackTrace();
 			request.setAttribute("error", "Error connecting to database (check console)");
@@ -136,17 +169,19 @@ public class Project3 extends HttpServlet {
 	
 	protected void loadPatients(HttpServletRequest request) {
 		try {
-			PreparedStatement st = conn.prepareStatement("SELECT (id,result,pred) FROM patients.patients;");
+			PreparedStatement st = conn.prepareStatement("SELECT id, result, pred FROM patients.patients ORDER BY id ASC");
 			ResultSet rs = st.executeQuery(); 
 			
-			StringBuilder table = new StringBuilder("<table>\n");
+			StringBuilder table = new StringBuilder("<table><tr><td>Patient ID</td><td>Result</td><td>Prediction</td></tr>\n");
 			
 			while (rs.next()) {
-				String id = rs.getString(0);
-				String result = rs.getString(1);
-				String pred = rs.getString(2);
+				String id = rs.getString(1);
+				String result = rs.getString(2);
+				String pred = rs.getString(3);
 				
-				table.append("<tr><td>").append(id).append("</td><td>").append(result).append("</td><td>").append(pred).append("</td></tr>\n");
+				table.append("<tr data-patient-id=\"").append(id).append("\"><td>")
+					.append(id).append("</td><td>").append(result).append("</td><td>")
+					.append(pred).append("</td></tr>\n");
 			}
 			
 			table.append("</table>\n");
@@ -165,6 +200,7 @@ public class Project3 extends HttpServlet {
 		if (conn != null) {
 			try {
 				conn.close();
+				conn = null;
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
