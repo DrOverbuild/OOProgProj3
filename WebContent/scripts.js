8718fe2b38163dddd2b49e4a539b76af6daab141
Jasper Reddin <jasper@jasperreddin.com>
2020-04-11 14:08:58 -0500

    Finish patient editor -- front and back end

--- WebContent/scripts.js
+++ WebContent/scripts.js
@@ -1,11 +1,90 @@
+var curPatient = null;
+
 $(document).ready(() => {
 	$('tr').on('click', function() {
 		var target = $(this);
 		var id = target.attr("data-patient-id");
 		
+		curPatient = id;
+		
 		$.get('./GetPatient?id=' + id, function(data) {
 			$('#rightcolumn').html(data).show();
+			setupPatientEditor();
 		});
 		
 	});
+	
+	$('.right-aligned-nav-button').click( () => {
+		$.get('./GetPatient', function(data) {
+			$('#rightcolumn').html(data).show();
+			setupPatientEditor();
+		});
+	});
 });
+
+function setupPatientEditor() {
+	$('.delete').click((e) => {
+		e.preventDefault()
+		deletePatient();
+	});
+	
+	$('.save').click((e) => {
+		e.preventDefault();
+		savePatient();
+	});
+	
+	$('.predict').click((e) => {
+		e.preventDefault();
+		predict();
+	});
+}
+
+function deletePatient() {
+	if (curPatient) {
+		var confirmed = confirm("Are you sure you want to delete this patient?")
+		
+		if (confirmed) {
+			$.get('./GetPatient?id=' + curPatient + "&delete", function(data) {
+				alert("Patient deleted");
+				location.reload(true);
+			});
+		}
+	}
+}
+
+function savePatient() {
+	if (curPatient) {
+		var result = $('#result').val();
+		var pred = $('#pred').val();
+		
+		$.get('./GetPatient?id=' + curPatient + "&save&result=" + result + "&pred=" + pred, function(data) {
+			location.reload(true);
+		});
+	} else {
+		var id = $('#id').val()
+		var result = $('#result').val();
+		var pred = $('#pred').val();
+		var proteins = $('#proteins').val();
+		
+		var data = {
+				add: true,
+				id: id,
+				result: result,
+				pred: pred,
+				proteins: proteins
+			};
+		
+		$.post('./GetPatient', data, function(data) {
+			location.reload(true);
+		});
+	}
+}
+
+function predict() {
+	if (curPatient) {
+		$.get('./GetPatient?id=' + curPatient + "&predict", function(data) {
+			alert("Patient updated");
+			location.reload(true);
+		});
+	}
+}
--- src/proj3/GetPatient.java
+++ src/proj3/GetPatient.java
@@ -39,13 +39,20 @@ public class GetPatient extends HttpServlet {
 		} 
 	}
 	
-	protected Patient loadPatient(String id) {
+	protected Patient loadPatient(String id, boolean includeProteins) {
 		if (conn == null) {
 			return null;
 		}
 		
 		try {
-			PreparedStatement statement = conn.prepareStatement("SELECT id,result,pred FROM patients WHERE id = ?");
+			
+			String proteinsAddition = "";
+			
+			if (includeProteins) {
+				proteinsAddition = ",proteins";
+			}
+			
+			PreparedStatement statement = conn.prepareStatement("SELECT id,result,pred" + proteinsAddition + " FROM patients WHERE id = ?");
 			int idInt = Integer.parseInt(id);
 			statement.setInt(1, idInt);
 			ResultSet rs = statement.executeQuery();
@@ -53,8 +60,20 @@ public class GetPatient extends HttpServlet {
 			if  (rs.last()) {
 				String result = rs.getString(2);
 				String pred = rs.getString(3);
+				double[] proteins = new double[]{};
 				
-				Patient patient = new Patient(result, pred, id, new double[]{}); // proteins are irrelevant right now
+				if (includeProteins) {
+					proteins = new double[4776];
+					String[] proteinsStr = rs.getString(4).split(",");
+					for (int i = 0; i < proteins.length; i++) {
+						proteins[i] = Double.parseDouble(proteinsStr[i]);
+					}
+				}
+				
+				Patient patient = new Patient(result, pred, id, proteins);
+				
+				statement.close();
+				rs.close();
 				
 				return patient;
 			}
@@ -65,6 +84,60 @@ public class GetPatient extends HttpServlet {
 		}
 		
 		return null;
+	}
+	
+	protected void updatePatient(Patient p) {
+		if (conn == null) {
+			return;
+		}
+		
+		try {
+			PreparedStatement st = conn.prepareStatement("UPDATE patients SET result = ?, pred = ? WHERE id = ?");
+			st.setString(1, p.getTreatmentResults());
+			st.setString(2, p.getPrediction());
+			
+			int idInt = Integer.parseInt(p.getPatientID());
+			
+			st.setInt(3, idInt);
+			
+			st.executeUpdate();
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	protected void deletePatient(String id) {
+		if (conn == null) {
+			return;
+		}
+		
+		try {
+			PreparedStatement statement = conn.prepareStatement("DELETE FROM patients WHERE id = ?");
+			int idInt = Integer.parseInt(id);
+			statement.setInt(1, idInt);
+			
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	protected void insertPatient(String id, String result, String pred, String proteins) {
+		if (conn == null) {
+			return;
+		}
+		
+		try {
+			PreparedStatement statement = conn.prepareStatement("INSERT INTO patients (id, result, pred, proteins) VALUES (?, ?, ?, ?)");
+			statement.setString(1, id);
+			statement.setString(2, result);
+			statement.setString(3, pred);
+			statement.setString(4, proteins);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
 	}
 	
     /**
@@ -81,15 +154,75 @@ public class GetPatient extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		if (request.getSession().getAttribute("signed_in") != null && (boolean)request.getSession().getAttribute("signed_in")) {
+			
+			if (request.getParameter("id")==null) {
+				response.getWriter().append("<form class=\"small\">\n" + 
+						" 			<div class=\"form-line-small\">\n" + 
+						" 				<label for=\"id\">ID:</label>\n" + 
+						" 				<input type=\"text\" id=\"id\" name=\"id\"></input>\n" + 
+						" 			</div>\n" + 
+						" 			<div class=\"form-line-small\">\n" + 
+						" 				<label for=\"result\">Result:</label>\n" + 
+						" 				<input id=\"result\" name=\"result\" type=\"text\"></input>\n" + 
+						" 			</div>\n" + 
+						" 			<div class=\"form-line-small\">\n" + 
+						" 				<label for=\"pred\">Prediction:</label>\n" + 
+						" 				<input  id=\"pred\" name=\"pred\" type=\"text\"></input>\n" + 
+						" 			</div>\n" + 
+						" 			<div class=\"form-line-small\">\n" + 
+						" 				<label for=\"proteins\">Proteins:</label>\n" + 
+						" 				<textarea rows=\"4\" id=\"proteins\" name=\"proteins\"></textarea>\n" + 
+						" 			</div>\n" + 
+						" 			<div class=\"form-line-small\">\n" + 
+						" 				<button class=\"save\">Save</button>\n" + 
+						" 			</div>\n" + 
+						" 		</form>");
+				return;
+			}
+			
+			makeDBConnection();
+			String id = "";
+			
 			if (request.getParameter("id") != null) {
-				String id = (String)request.getParameter("id");
-				makeDBConnection();
-				Patient patient = loadPatient(id);
+				id = (String)request.getParameter("id");
+			} else {
+				response.getWriter().append("Please provide patient id.");
+				return;
+			}
+			
+			if (request.getParameter("predict") != null) {
+				Patient patient = loadPatient(id, true);
+				patient.setPrediction(patientpredictor.Predictor.predict(patient));
+				updatePatient(patient);
+			} else if (request.getParameter("delete") != null) {
+				deletePatient(id);
+			} else if (request.getParameter("save") != null) {
+				String result;
+				String pred;
 				
+				if (request.getParameter("result") != null) {
+					result = (String)request.getParameter("result");
+				} else {
+					response.getWriter().append("Please provide all the parameters.");
+					return;
+				}
+				
+				if (request.getParameter("pred") != null) {
+					pred = (String)request.getParameter("pred");
+				} else {
+					response.getWriter().append("Please provide all the parameters.");
+					return;
+				}
+				
+				
+				Patient patient = new Patient(result, pred, id, new double[]{});
+				updatePatient(patient);
+			} else {
+				Patient patient = loadPatient(id, false);
 				if (patient == null) {
 					response.getWriter().append("Could not load patient");
 				} else {
-					response.getWriter().append("<form class=\"small\">\n" + 
+					response.getWriter().append("<form class=\"small\">\n" +
 						" 			<div class=\"form-line-small\">\n" + 
 						" 				<label for=\"id\">ID:</label>\n" + 
 						" 				<input value=\"" + id + "\" type=\"text\" id=\"id\" name=\"id\"></input>\n" + 
@@ -120,6 +253,70 @@ public class GetPatient extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		if (request.getSession().getAttribute("signed_in") != null && (boolean)request.getSession().getAttribute("signed_in")) {
+			if (request.getParameter("add") != null) {
+				String id = "";
+				String result = "";
+				String pred = "";
+				String proteinsStr = "";
+				
+				if (request.getParameter("id") != null) {
+					id = (String)request.getParameter("id");
+				} else {
+					response.getWriter().append("Please provide all the parameters.");
+					return;
+				}
+				
+				if (request.getParameter("result") != null) {
+					result = (String)request.getParameter("result");
+					
+					if (result.isEmpty()) {
+						result = "unknown";
+					}
+				} else {
+					response.getWriter().append("Please provide all the parameters.");
+					return;
+				}
+				
+				
+				if (request.getParameter("pred") != null) {
+					pred = (String)request.getParameter("pred");
+					
+					if (pred.isEmpty()) {
+						pred = "unknown";
+					}
+				} else {
+					response.getWriter().append("Please provide all the parameters.");
+					return;
+				}
+				
+				if (request.getParameter("proteins") != null) {
+					proteinsStr = (String)request.getParameter("proteins");
+				} else {
+					response.getWriter().append("Please provide all the parameters.");
+					return;
+				}
+				
+				
+				String[] proteinsStrArr = proteinsStr.split(",");
+				if (proteinsStrArr.length != 4776) {
+					response.getWriter().append("Not enough proteins.");
+					return;
+				}
+				
+				makeDBConnection();
+				
+				insertPatient(id, result, pred, proteinsStr);
+				response.getWriter().append("Success");
+				
+			} else {
+				response.getWriter().append("Please add the 'add' parameter.");
+			}
+		} else {
+			response.getWriter().append("Not Authorized");
+		}
+		
+//		doGet(request, response);
 	}
 }
